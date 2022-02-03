d375f6d8b593c334de3d61b4b80510ea020e7ef9
Shawn Varghese <svarghs8531@gmail.com>
2022-02-02 23:47:47 -0500

    Renamed file and finished code.

--- screen_battle.py
+++ screen_battle.py
@@ -22,9 +22,29 @@ class Screen_Battle (tkinter.Frame):
         '''
         This method creates all of the (initial) widgets for the battle page.
         '''
-        #
-        # TO DO
-        #
+        self.attack_bttn = tkinter.Button(self, text="Attack", command=self.attack_clicked)
+        self.attack_bttn.grid(row=0,column=0)
+        self.attack_output1 = tkinter.Label(self)
+        self.attack_output1.grid(row=0,column=1)
+        self.attack_output2 = tkinter.Label(self)
+        self.attack_output2.grid(row=1,column=1)
+        self.victory_label = tkinter.Label(self)
+        self.victory_label.grid(row=2,column=1)
+        text_labels = ["You", "Computer", f"{self.player1.hit_points}/{self.player1_max_hp} HP", f"{self.player2.hit_points}/{self.player2_max_hp} HP"]
+        players = [self.player1, self.player2]
+        self.hit_points_labels = []
+        for i in range(1, 4):
+            for j in range(2):
+                if i == 2:
+                    imageLarge = tkinter.PhotoImage(file="images/" + players[j].large_image)
+                    w = tkinter.Label (self, image = imageLarge)
+                    w.photo = imageLarge
+                    w.grid(row=i+2, column=j)
+                elif i == 3:
+                    self.hit_points_labels.append(tkinter.Label(self, text=text_labels[i+j-1]))
+                    self.hit_points_labels[j].grid(row=i+2,column=j)
+                else:
+                    tkinter.Label(self, text=text_labels[i+j-1]).grid(row=i+2,column=j)
         
     def attack_clicked(self):
         ''' This method is called when the user presses the "Attack" button.
@@ -38,17 +58,24 @@ class Screen_Battle (tkinter.Frame):
             To remove a widget, use the destroy() method. For example:
     
                 self.button.destroy()   
-        '''        
-        #
-        # TO DO
-        #
-                                            
+        '''
+        self.attack_output1["text"] = self.player1.attack(self.player2)
+        self.hit_points_labels[1]["text"] = f"{self.player2.hit_points}/{self.player2_max_hp} HP"
+        if self.player2.hit_points > 0:
+            self.attack_output2["text"] = self.player2.attack(self.player1)
+            self.hit_points_labels[0]["text"] = f"{self.player1.hit_points}/{self.player1_max_hp} HP"
+        else:
+            self.attack_output2.destroy()
+            self.victory_update(self.player1)
+        if self.player1.hit_points < 0:
+            self.victory_update(self.player2)
+                                           
     def exit_clicked(self):
         ''' This method is called when the Exit button is clicked. 
             It passes control back to the callback method. '''        
         self.callback_on_exit()
-  
-            
-            
-            
-            
+
+    def victory_update(self, player):
+        self.victory_label["text"] = f"{player.name} is victorious!"
+        self.attack_bttn.destroy()
+        tkinter.Button(self, text="Exit!",command=self.exit_clicked).grid(row=7,column=1,sticky=tkinter.E)
--- screen_prepare_to_battle.py
+++ screen_prepare_to_battle.py
@@ -1,4 +1,5 @@
 import tkinter
+import characters
 
 class Screen_PrepareToBattle (tkinter.Frame):
     def __init__ (self, master, player1, player2, callback_on_commence_battle):
@@ -13,19 +14,33 @@ class Screen_PrepareToBattle (tkinter.Frame):
         
         self.create_widgets()
         self.grid()
-        
     
-    def create_widgets (self):
-         '''
+    def create_widgets(self):
+        '''
         This method creates all of the widgets the prepare to battle page.
         '''
-        #
-        # TO DO
-        #
- 
+        labels = ["You", "Computer", f"{self.player1.hit_points} HP", f"{self.player2.hit_points} HP", f"{self.player1.dexterity} Dexterity", f"{self.player2.dexterity} Dexterity", f"{self.player1.strength} Strength", f"{self.player2.strength} Strength"]
+        players = [self.player1, self.player2]
+        i = 0
+        count = 0
+        while count < 5:
+            for j in range(2):
+                if count == 1:
+                    imageLarge = tkinter.PhotoImage(file="images/" + players[j].large_image)
+                    w = tkinter.Label (self, image = imageLarge)
+                    w.photo = imageLarge
+                    w.grid(row=count, column=j)
+                    i -= 1
+                else:
+                    tkinter.Label(self, text=labels[i]).grid(row=count,column=j)
+                i += 1
+            count += 1
+        
+        tkinter.Button(self, text="Commence Battle!", command=self.commence_battle_clicked).grid(row=5,column=1,sticky=tkinter.E)
+
+                
+
     def commence_battle_clicked(self):
         ''' This method is called when the Battle button is clicked. 
             It passes control back to the callback method. '''         
         self.callback_on_commence_battle()
-            
-        
